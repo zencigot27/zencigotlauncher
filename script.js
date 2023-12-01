2e6b6e38f5563506feff0ffdc9ab73ccd4a228d4
zencigot27 <149002388+zencigot27@users.noreply.github.com>
2023-12-01 11:02:47 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function buttonclick(){
+    alert("Ben öyle yazılımcı fln değilim, OK'a bas tlauncher indir. Ayrıca o da ücretsiz ne farkı var.");
+    window.location.href = 'https://tlauncher.org/en/';
+}
